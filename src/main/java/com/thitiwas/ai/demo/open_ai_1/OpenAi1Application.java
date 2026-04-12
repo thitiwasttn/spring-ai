b95895ef831bc13e850485d369a89eacc9e229e6
Thitiwas Nupan <55071973+thitiwasttn@users.noreply.github.com>
2026-04-12 20:07:31 +0700

    initial project

---
+++ src/main/java/com/thitiwas/ai/demo/open_ai_1/OpenAi1Application.java
@@ -0,0 +1,13 @@
+package com.thitiwas.ai.demo.open_ai_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenAi1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenAi1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/thitiwas/ai/demo/open_ai_1/OpenAi1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thitiwas.ai.demo.open_ai_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenAi1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
